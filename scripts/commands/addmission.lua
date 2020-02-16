8b6b2aa216d57fc6254d317f283958a2fb77364d
Brian Silvi <lunaofthemoon@gmail.com>
2020-02-15 19:47:57 -0500

    Added info header to addmission

--- scripts/commands/addmission.lua
+++ scripts/commands/addmission.lua
@@ -1,6 +1,24 @@
 ---------------------------------------------------------------------------------------------------
 -- func: addmission <logID> <missionID> <player>
 -- desc: Adds a mission to the GM or target players log.
+-- https://wiki.dspt.info/index.php/Log_IDs#Mission_Log_IDs
+
+
+-- Mission Log ID	Mission Log Name	Full Name
+-- 			0		SANDORIA			Sandoria
+-- 			1		BASTOK				Bastok
+-- 			2		WINDURST			Windurst
+-- 			3		ZILART				Rise of the Zilart
+-- 			4		TOAU				Treasures of Aht Urhgan
+-- 			5		WOTG				Wings of the Goddess
+-- 			6		COP					Chains of Promathia
+-- 			7		ASSAULT				Assault
+-- 			8		CAMPAIGN			Campaign
+-- 			9		ACP					A Crystalline Prophecy
+-- 			10		AMK					A Moogle Kupo d'Etat
+-- 			11		ASA					A Shantotto Ascension
+-- 			12		SOA					Seekers of Adoulin
+-- 			13		ROV					Rhapsodies of Vana'diel
 ---------------------------------------------------------------------------------------------------
 
 require("scripts/globals/missions");
@@ -27,7 +45,7 @@ function onTrigger(player, logId, missionId, target)
     end
     logName = logInfo.full_name;
     logId = logInfo.mission_log;
-    
+
     -- validate missionId
     local areaMissionIds = tpz.mission.id[tpz.mission.area[logId]]
     if (missionId ~= nil) then
