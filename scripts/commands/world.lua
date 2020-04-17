0e4ce336ded2da97affbcc3660dc24975b3f281c
Brian Silvi <lunaofthemoon@gmail.com>
2020-04-17 15:36:31 -0400

    Added World Command
    This allows people to use !w or !world to talk to the entire server, eliminating the need for a server LS

---
+++ scripts/commands/world.lua
@@ -0,0 +1,33 @@
+---------------------------------------------------------------------------------------------------
+-- func: w, world
+-- desc: prints a message to the world
+---------------------------------------------------------------------------------------------------
+
+cmdprops =
+{
+    permission = 0,
+    parameters = "s"
+}
+
+function error(player, msg)
+    player:PrintToPlayer(msg)
+    player:PrintToPlayer("!world <message>")
+end
+
+function onTrigger(player, msg)
+    if msg == nil then
+        error(player, "Cannot send an empty message because then it's not a message, it's just you fucking around.")
+    else
+        if player:getGMLevel() == 2 then
+            player:PrintToArea(string.format("[World] [GM]%s: %s", player:getName(), msg), tpz.msg.channel.NS_PARTY, tpz.msg.area.SERVER, "No One")
+        elseif player:getGMLevel() == 3 then
+            player:PrintToArea(string.format("[World] [SGM]%s: %s", player:getName(), msg), tpz.msg.channel.NS_PARTY, tpz.msg.area.SERVER, "No One")
+        elseif player:getGMLevel() == 4 then
+            player:PrintToArea(string.format("[World] [LGM]%s: %s", player:getName(), msg), tpz.msg.channel.NS_PARTY, tpz.msg.area.SERVER, "No One")
+        elseif player:getGMLevel() == 5 then
+            player:PrintToArea(string.format("[World] [SO]%s: %s", player:getName(), msg), tpz.msg.channel.NS_PARTY, tpz.msg.area.SERVER, "No One")
+        else
+            player:PrintToArea(string.format("[World] %s: %s", player:getName(), msg), tpz.msg.channel.NS_PARTY, tpz.msg.area.SERVER, "No One")
+        end
+    end
+end
