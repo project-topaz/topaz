334b0efd27d6b5e8af208f38e9d2ddf24f9c443e
Brian Silvi <lunaofthemoon@gmail.com>
2020-04-24 11:17:48 -0400

    Increased gil rate and added ore to reet

--- scripts/zones/Bastok_Markets/npcs/Reet.lua
+++ scripts/zones/Bastok_Markets/npcs/Reet.lua
@@ -18,12 +18,20 @@ function onTrigger(player,npc)
     {
 		4096,	102, 3,	-- Fire Crystal
 		4097,	307, 2,	-- Ice Crystal
-		4098,	99, 3,	-- Wind Crystal
+		4098,	99,  3,	-- Wind Crystal
 		4099,	102, 3,	-- Earth Crystal
 		4100,	307, 2,	-- Lightning Crystal
 		4101,	116, 3,	-- Water Crystal
 		4102,	718, 1,	-- Light Crystal
 		4103,	589, 1,	-- Dark Crystal
+        1255, 12324, 2, -- Fire Ore
+        1256, 12324, 2, -- Ice Ore
+        1257, 12324, 2, -- Wind Ore
+        1258, 12324, 2, -- Earth Ore
+        1259, 12324, 2, -- Lightning Ore
+        1260, 12324, 2, -- Water Ore
+        1261, 12324, 2, -- Light Ore
+        1262, 12324, 2, -- Dark Ore
     }
 
     tpz.shop.nation(player, stock, tpz.nation.BASTOK)
