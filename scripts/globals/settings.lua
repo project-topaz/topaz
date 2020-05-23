d1e44551957b9a6974280a66dcea87065348c8e6
Brian Silvi <lunaofthemoon@gmail.com>
2020-05-23 19:52:16 -0400

    Increased casket rate

--- scripts/globals/settings.lua
+++ scripts/globals/settings.lua
@@ -36,7 +36,7 @@ REGIME_WAIT = 0; -- Make people wait till 00:00 game time as in retail. If it's 
 -- Retail droprate = 0.1 (10%) with no other effects active
 -- Set to 0 to disable caskets.
 -- max is clamped to 1.0 (100%)
-CASKET_DROP_RATE = 0.11;
+CASKET_DROP_RATE = 0.20;
 
 -- Setting to lock content more accurately to the content you have defined above
 -- This generally results in a more accurate presentation of your selected expansions
