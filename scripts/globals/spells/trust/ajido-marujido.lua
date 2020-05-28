e983d4a48a8f60f91f65f4a2bca2176bdb03d2d9
Zach Toogood <zrtoogood@gmail.com>
2020-05-28 19:41:38 +0300

    More trusts

--- scripts/globals/spells/trust/ajido-marujido.lua
+++ scripts/globals/spells/trust/ajido-marujido.lua
@@ -1,13 +1,30 @@
 -----------------------------------------
 -- Trust: Ajido-Marujido
 -----------------------------------------
+require("scripts/globals/magic")
+require("scripts/globals/gambits")
+require("scripts/globals/status")
 require("scripts/globals/trust")
 -----------------------------------------
 
 function onMagicCastingCheck(caster, target, spell)
-    return tpz.trust.canCast(caster, spell)
+    return tpz.trust.canCast(caster, spell, 1019)
 end
 
 function onSpellCast(caster, target, spell)
     return tpz.trust.spawn(caster, spell)
+end
+
+function onMobSpawn(mob)
+    mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 25,
+                        ai.r.MA, ai.s.HIGHEST, tpz.magic.spellFamily.CURE)
+
+    mob:addSimpleGambit(ai.t.TARGET, ai.c.MB_AVAILABLE, 0,
+                        ai.r.MA, ai.s.MB_ELEMENT, tpz.magic.spellFamily.NONE)
+
+    mob:addSimpleGambit(ai.t.TARGET, ai.c.NOT_SC_AVAILABLE, 0,
+                        ai.r.MA, ai.s.HIGHEST, tpz.magic.spellFamily.NONE, 30)
+
+    mob:addSimpleGambit(ai.t.TARGET, ai.c.NOT_STATUS, tpz.effect.SLOW,
+                        ai.r.MA, ai.s.HIGHEST, tpz.magic.spellFamily.SLOW, 60)
 end
