1af93f1b65b5bd32e669a34b5dca913b9d2e1d32
Brian Silvi <lunaofthemoon@gmail.com>
2020-05-31 10:19:16 -0400

    Warp II can no longer be cast on trusts.

--- scripts/globals/spells/warp_ii.lua
+++ scripts/globals/spells/warp_ii.lua
@@ -8,11 +8,18 @@ require("scripts/globals/msg")
 -----------------------------------------
 
 function onMagicCastingCheck(caster,target,spell)
-    return 0
+    if target:isPC() then
+        return 0
+    else
+        return tpz.msg.basic.MAGIC_CANNOT_BE_CAST
+    end
+
 end
 
 function onSpellCast(caster,target,spell)
-    target:addStatusEffectEx(tpz.effect.TELEPORT,0,tpz.teleport.id.WARP,0,3.4)
-    spell:setMsg(tpz.msg.basic.MAGIC_TELEPORT)
-    return 0
+    if target:isPC() then
+        target:addStatusEffectEx(tpz.effect.TELEPORT,0,tpz.teleport.id.WARP,0,3.4)
+        spell:setMsg(tpz.msg.basic.MAGIC_TELEPORT)
+        return 0
+    end
 end
