ccd80f77ce8f6d3832fd8ae3f4a1cd5292911405
Zach Toogood <zrtoogood@gmail.com>
2020-06-13 20:36:22 +0300

    IDs and TODOs

--- scripts/zones/Batallia_Downs/IDs.lua
+++ scripts/zones/Batallia_Downs/IDs.lua
@@ -52,7 +52,9 @@ zones[tpz.zone.BATALLIA_DOWNS] =
     },
     npc =
     {
-        CASKET_BASE   = 17207794,
+        CASKET_BASE      = 17207794,
+        BLUE_BEAM_BASE   = 17207972,
+        RAPTOR_FOOD_BASE = 17207980,
     },
 }
 
--- scripts/zones/Batallia_Downs/Zone.lua
+++ scripts/zones/Batallia_Downs/Zone.lua
@@ -17,6 +17,17 @@ end;
 function onInitialize(zone)
     UpdateNMSpawnPoint(ID.mob.AHTU);
     GetMobByID(ID.mob.AHTU):setRespawnTime(math.random(900, 10800));
+
+    -- TODO: If the circle is infinite height, use a cube
+    zone:registerRegion(1, 357.819, 11, -250.201, 0, 0, 0)
+    zone:registerRegion(2, 357.819, 11, -250.201, 0, 0, 0)
+    zone:registerRegion(3, 357.819, 11, -250.201, 0, 0, 0)
+    zone:registerRegion(4, 357.819, 11, -250.201, 0, 0, 0)
+    zone:registerRegion(5, 357.819, 11, -250.201, 0, 0, 0)
+    zone:registerRegion(6, 357.819, 11, -250.201, 0, 0, 0)
+    zone:registerRegion(7, 357.819, 11, -250.201, 0, 0, 0)
+    zone:registerRegion(8, 357.819, 11, -250.201, 0, 0, 0)
+
 end;
 
 function onZoneIn( player, prevZone)
@@ -32,14 +43,35 @@ function onZoneIn( player, prevZone)
         cs = 903;
     end
 
+    -- TODO: If Full Speed Ahead, send enable packet
+    -- Also add FSA status, ticking info
+
     return cs;
 end;
 
 function onConquestUpdate(zone, updatetype)
     tpz.conq.onConquestUpdate(zone, updatetype)
-end;
+end
+
+local function fsaHelper(player, offset)
+    -- If quest status FSA etc.
+    -- TODO: remove from map
+    -- ID.npc.BLUE_BEAM_BASE + offset
+    -- ID.npc.RAPTOR_FOOD_BASE + offset
+end
 
 function onRegionEnter( player, region)
+    switch (region:GetRegionID()): caseof
+    {
+        [1] = function(x) fsaHelper(player, 0) end,
+        [2] = function(x) fsaHelper(player, 1) end,
+        [3] = function(x) fsaHelper(player, 2) end,
+        [4] = function(x) fsaHelper(player, 3) end,
+        [5] = function(x) fsaHelper(player, 4) end,
+        [6] = function(x) fsaHelper(player, 5) end,
+        [7] = function(x) fsaHelper(player, 6) end,
+        [8] = function(x) fsaHelper(player, 7) end,
+    }
 end;
 
 function onEventUpdate( player, csid, option)
