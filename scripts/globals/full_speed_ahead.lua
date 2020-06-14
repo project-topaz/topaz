c5c487030bc25a29f21271e8079420d530ce2cd7
Zach Toogood <zrtoogood@gmail.com>
2020-06-14 12:12:09 +0300

    Store and use food_byte

--- scripts/globals/full_speed_ahead.lua
+++ scripts/globals/full_speed_ahead.lua
@@ -12,6 +12,8 @@ tpz.full_speed_ahead.onZoneIn = function(player)
     player:setLocalVar("FSA_Time", os.time() + 600)
     player:setLocalVar("FSA_Motivation", 100)
     player:setLocalVar("FSA_Pep", 100)
+    player:setLocalVar("FSA_Food", 0xFF)
+    player:setLocalVar("FSA_FoodCount", 0)
 
     player:addStatusEffectEx(tpz.effect.MOUNTED, tpz.effect.MOUNTED, tpz.mount.QUEST_RAPTOR, 2, 0, true)
 end
@@ -22,38 +24,35 @@ tpz.full_speed_ahead.tick = function(player)
     local timeLeft = player:getLocalVar("FSA_Time") - os.time()
     local motivation = player:getLocalVar("FSA_Motivation")
     local pep = player:getLocalVar("FSA_Pep")
+    local food_byte = player:getLocalVar("FSA_Food")
+    local food_count = player:getLocalVar("FSA_FoodCount")
 
-    player:countdown(timeLeft, "Motivation", motivation, "Pep", pep)
-
-    -- TODO: Store me
-    local entity_data = {}
+    local food_data = {}
     for i = 0, 7 do 
-        table.insert(entity_data, ID.npc.BLUE_BEAM_BASE + i)
-        table.insert(entity_data, ID.npc.RAPTOR_FOOD_BASE + i) 
+        if bit.band(player:getLocalVar("FSA_Food"), bit.lshift(1, i)) then
+            table.insert(food_data, ID.npc.BLUE_BEAM_BASE + i)
+            table.insert(food_data, ID.npc.RAPTOR_FOOD_BASE + i)
+        end
     end
-    player:enableEntities(entity_data)
 
     if motivation == 0 then
-        player:countdown(0)
         player:delStatusEffect(tpz.effect.MOUNTED)
+        player:countdown(0) 
         player:enableEntities({})
+    else
+        player:countdown(timeLeft, "Motivation", motivation, "Pep", pep)
+        player:enableEntities(food_data)
     end
 end
 
 tpz.full_speed_ahead.onRegionEnter = function(player, index)
-    printf("onRegionEnter")
+    local food_byte = player:getLocalVar("FSA_Food")
 
-    -- TODO: Store me
-    local entity_data = {}
-    for i = 0, 7 do 
-        table.insert(entity_data, ID.npc.BLUE_BEAM_BASE + i)
-        table.insert(entity_data, ID.npc.RAPTOR_FOOD_BASE + i) 
+    if bit.band(food_byte, bit.lshift(1, index - 1)) then
+        local new_food_byte = food_byte - bit.lshift(1, index - 1)
+        player:setLocalVar("FSA_Food", new_food_byte)
+        player:setLocalVar("FSA_FoodCount", player:getLocalVar("FSA_FoodCount") + 1)
     end
-
-    entity_data[index] = 0
-    entity_data[index + 8] = 0
-
-    player:enableEntities(entity_data)
 end
 
 tpz.fsa = tpz.full_speed_ahead
