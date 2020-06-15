e5010b42a754aa242eb7da483a3c7493d763323d
Brian Silvi <lunaofthemoon@gmail.com>
2020-06-15 11:44:37 -0400

    Fixed spawn window on HAWKEYED_DNATBAT

--- scripts/zones/Davoi/mobs/Orcish_Cursemaker.lua
+++ scripts/zones/Davoi/mobs/Orcish_Cursemaker.lua
@@ -11,5 +11,5 @@ function onMobDeath(mob, player, isKiller)
 end
 
 function onMobDespawn(mob)
-    tpz.mob.phOnDespawn(mob, ID.mob.HAWKEYED_DNATBAT_PH, 10, 3600) -- 1 hour
+    tpz.mob.phOnDespawn(mob, ID.mob.HAWKEYED_DNATBAT_PH, 10, math.random(3600,10800)) -- 1-3 hours.
 end
