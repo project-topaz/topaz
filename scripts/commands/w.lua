50ea7ae2af0ab06072c6671809f7f2e2c398cce4
Brian Silvi <lunaofthemoon@gmail.com>
2020-06-26 13:27:31 -0400

    Fixed world chat

--- scripts/commands/w.lua
+++ scripts/commands/w.lua
@@ -3,34 +3,32 @@
 -- desc: prints a message to the world
 ---------------------------------------------------------------------------------------------------
 
+require("scripts/globals/commands")
+
 cmdprops =
 {
     permission = 0,
     parameters = "s"
 }
 
-function error(player, msg)
-    player:PrintToPlayer(msg)
-    player:PrintToPlayer("!w <message>")
-end
+function onTrigger(caller, player, msg)
+    local usage = "!w <message>"
 
-function onTrigger(player, msg)
-    if msg == nil then
-        error(player, "Cannot send an empty message because then it's not a message, it's just you fucking around.")
+    if (msg == nil) then
+        tpz.commands.error(caller, player, "Cannot send an empty message because then it's not a message, it's just you fucking around.", usage)
     else
-        if player:getGMLevel() == 2 then
-            player:PrintToArea(string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, string.format("[GM]%s", player:getName()))
-        elseif player:getGMLevel() == 3 then
-            player:PrintToArea(string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, string.format("[SGM]%s", player:getName()))
-        elseif player:getGMLevel() == 4 then
-            player:PrintToArea(string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, string.format("[LGM]%s", player:getName()))
-        elseif player:getGMLevel() == 5 then
-            player:PrintToArea(string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, string.format("[DEV]%s", player:getName()))
-        elseif player:getGMLevel() >= 6 then
-            player:PrintToArea(string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, string.format("[SO]%s", player:getName()))
+        if (player:getGMLevel() == 2) then
+            tpz.commands.print(caller, player, string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, string.format("[GM]%s", player:getName()))
+        elseif (player:getGMLevel() == 3) then
+            tpz.commands.print(caller, player, string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, string.format("[SGM]%s", player:getName()))
+        elseif (player:getGMLevel() == 4) then
+            tpz.commands.print(caller, player, string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, string.format("[LGM]%s", player:getName()))
+        elseif (player:getGMLevel() == 5) then
+            tpz.commands.print(caller, player, string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, string.format("[DEV]%s", player:getName()))
+        elseif (player:getGMLevel() >= 6) then
+            tpz.commands.print(caller, player, string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, string.format("[SO]%s", player:getName()))
         else
-            player:PrintToArea(string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, player:getName())
+            tpz.commands.print(caller, player, string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, player:getName())
         end
-
     end
 end
