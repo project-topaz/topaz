c0c85e0b70215f0b02f75e1470e2a882eb394f08
Xaver-DaRed <60053999+Xaver-DaRed@users.noreply.github.com>
2020-10-12 19:10:20 +0200

    Skill up changes.
    Added skill up over recipe level, as the prophethy foretold.
    "Cleaned" the code

--- src/map/map.cpp
+++ src/map/map.cpp
@@ -995,7 +995,7 @@ int32 map_config_default()
     map_config.mount_speed_mod = 0;
     map_config.mob_speed_mod = 0;
     map_config.skillup_chance_multiplier = 1.0f;
-    map_config.craft_chance_multiplier = 1.0f;
+    map_config.craft_chance_multiplier = 1;
     map_config.skillup_amount_multiplier = 1;
     map_config.craft_amount_multiplier = 1;
     map_config.garden_day_matters = false;
@@ -1284,7 +1284,7 @@ int32 map_config_read(const int8* cfgName)
         }
         else if (strcmp(w1, "craft_chance_multiplier") == 0)
         {
-            map_config.craft_chance_multiplier = (float)atof(w2);
+            map_config.craft_chance_multiplier = atoi(w2);
         }
         else if (strcmp(w1, "skillup_amount_multiplier") == 0)
         {
--- src/map/map.h
+++ src/map/map.h
@@ -93,7 +93,7 @@ struct map_config_t
     int8   mount_speed_mod;           // Modifier to add to mount speed
     int8   mob_speed_mod;             // Modifier to add to monster speed
     float  skillup_chance_multiplier; // Constant used in the skillup formula that has a strong effect on skill-up rates
-    float  craft_chance_multiplier;   // Constant used in the crafting skill-up formula that has a strong effect on skill-up rates
+    uint8  craft_chance_multiplier;   // Constant used in the crafting skill-up formula that has a strong effect on skill-up rates
     float  skillup_amount_multiplier; // Used to increase the amount of skill gained during skill up
     float  craft_amount_multiplier;   // Used to increase the amount of skill gained during skill up
     bool   garden_day_matters;        // Enable/disable Element day factor in gardening results
