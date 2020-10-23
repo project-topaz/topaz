0ae51a0fc162eec849130a30de6871990b7647e4
bope <eric.hd.williams@gmail.com>
2020-10-23 06:26:19 -0700

    Simple patrol path fixes to make npc's follow paths more realibly

--- src/map/lua/lua_baseentity.cpp
+++ src/map/lua/lua_baseentity.cpp
@@ -1675,7 +1675,8 @@ inline int32 CLuaBaseEntity::atPoint(lua_State* L)
         lua_pop(L, 1);
     }
 
-    lua_pushboolean(L, m_PBaseEntity->loc.p.x == posX && m_PBaseEntity->loc.p.y == posY && m_PBaseEntity->loc.p.z == posZ);
+    lua_pushboolean(L, 1000. * m_PBaseEntity->loc.p.x == 1000. * posX && 1000. * m_PBaseEntity->loc.p.y == 1000. * posY &&
+                           1000. * m_PBaseEntity->loc.p.z == 1000. * posZ);
 
     return 1;
 }
@@ -14719,6 +14720,8 @@ Lunar<CLuaBaseEntity>::Register_t CLuaBaseEntity::methods[] =
     LUNAR_DECLARE_METHOD(CLuaBaseEntity,checkNameFlags),
     LUNAR_DECLARE_METHOD(CLuaBaseEntity,getModelId),
     LUNAR_DECLARE_METHOD(CLuaBaseEntity,setModelId),
+    LUNAR_DECLARE_METHOD(CLuaBaseEntity, getLookSize),
+    LUNAR_DECLARE_METHOD(CLuaBaseEntity, setLookSize),
     LUNAR_DECLARE_METHOD(CLuaBaseEntity,costume),
     LUNAR_DECLARE_METHOD(CLuaBaseEntity,costume2),
 
