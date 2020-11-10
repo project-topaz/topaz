237065f74434eab101e484067409c05db071de32
kaincenteno <kain.centeno@catsangel.com>
2020-11-09 21:26:48 -0800

    added table for NOVEMBER 2019. Only working for Moogle in Windurst Walls Works for talking and update.

--- scripts/globals/settings.lua
+++ scripts/globals/settings.lua
@@ -39,6 +39,10 @@ GOV_REWARD_ALLIANCE = 1 -- Allow Grounds of Valor rewards while being a member o
 ENABLE_ROE = 1
 ENABLE_ROE_TIMED = 1 -- Enable 4-hour timed records
 
+-- Login Campaign (Set to 0 if you don't want to run a Login Campaign)
+CAMPAIGN_YEAR = 2019
+CAMPAIGN_MONTH = 11
+
 -- TREASURE CASKETS
 -- Retail droprate = 0.1 (10%) with no other effects active
 -- Set to 0 to disable caskets.
