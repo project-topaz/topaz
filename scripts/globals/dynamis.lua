c52d8cfaad4736847189738a172ac803576b22fd
Marian Arlt <marianarlt@icloud.com>
2020-11-12 15:28:41 -0600

    fix currencyGoblinIntro not being set

--- scripts/globals/dynamis.lua
+++ scripts/globals/dynamis.lua
@@ -335,7 +335,7 @@ dynamis.entryNpcOnEventFinish = function(player, csid, option)
         npcUtil.giveKeyItem(player, tpz.ki.VIAL_OF_SHROUDED_SAND)
         player:setCharVar("Dynamis_Status", utils.mask.setBit(mask, 0, false))
         -- flag to make any currency goblin introduce prismatic hourglass
-        player:setCharVar("currencyGoblinIntro", utils.mask.setBit(introMask, 0, true))
+        player:setCharVar("currencyGoblinIntro", utils.mask.setBit(player:getCharVar("currencyGoblinIntro"), 0, true))
 
     -- first visit cutscene
     elseif info.csFirst and csid == info.csFirst then
